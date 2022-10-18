7df87a7908dfb643790d8d3b46f0a0a6362e45c6
Alejandro <torresxiaotorres@hotmail.com>
2022-10-18 11:06:57 -0500

    Limpieza Código

--- PokemonApp/Controller/ViewController.swift
+++ PokemonApp/Controller/ViewController.swift
@@ -1,16 +1,16 @@
 
 import UIKit
 class ViewController: UIViewController, UITableViewDelegate {
-
+    
     @IBOutlet weak var tableView: UITableView!
-
+    
     
     @IBOutlet weak var buttonOne: UIButton!
     @IBOutlet weak var buttonTwo: UIButton!
     @IBOutlet weak var buttonTree: UIButton!
-   
+    
     @IBOutlet weak var constrainCentre: NSLayoutConstraint!
-
+    
     @IBOutlet weak var elemento: UIImageView!
     
     @IBOutlet weak var elemento2: UIImageView?
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var vistaFeatures: UIView!
     @IBOutlet weak var namePokemonFeatures: UILabel!
     @IBOutlet weak var codigoPokemonFeatures: UILabel!
-
+    
     
     @IBOutlet weak var buttonViewT: UIButton!
     
@@ -45,19 +45,19 @@ class ViewController: UIViewController, UITableViewDelegate {
         gradient.type = .axial
         gradient.colors = [
             UIColor.init(named: "blueCustom")!.cgColor,
-           // UIColor.purple.cgColor,
+            // UIColor.purple.cgColor,
             UIColor.init(named: "greenCustom")!.cgColor
         ]
         gradient.startPoint = CGPoint(x: 0, y: 1)
         gradient.endPoint = CGPoint(x: 1, y: 1)
         return gradient
     }()
-
-
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
         self.navigationController?.isNavigationBarHidden = true
         
         searchBar.delegate = self
@@ -65,16 +65,16 @@ class ViewController: UIViewController, UITableViewDelegate {
         tableView.dataSource = self
         tableView.delegate = self
         pokemonManager.delegate = self
-    
+        
         tableView.register(UINib(nibName: "PokemonCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
-
+        
         let tittle = "Pokemon"
         title = tittle
         
         vistaFeatures.layer.cornerRadius = 10
         
-            gradient.frame = view.bounds
-            self.view.layer.insertSublayer(gradient, at:0)
+        gradient.frame = view.bounds
+        self.view.layer.insertSublayer(gradient, at:0)
         
         
         loadItems()
@@ -88,9 +88,7 @@ class ViewController: UIViewController, UITableViewDelegate {
         vistaFeatures.alpha = 0
         buttonViewT.alpha = 0
         
-    
-        setGradientBackground()
-                 
+        
         buttonOne.setImage(UIImage(named: "pikachuicono"), for: .normal)
         buttonTwo.setImage(UIImage(named: "localizacion"), for: .normal)
         buttonTree.setImage(UIImage(named: "dulce"), for: .normal)
@@ -113,21 +111,21 @@ class ViewController: UIViewController, UITableViewDelegate {
         
         if tableView.indexPathForSelectedRow != nil {
             if let namePoke = namePokemonFeatures.text, let imagenSegue = featuresTotal.image, let elementIcon = elemento.image {
-            destinationVC.selectedPokemon = namePoke
-            destinationVC.selectedPokemonImage = imagenSegue
-            destinationVC.selectedPokemonIconoElement = elementIcon
-            destinationVC.selectedPokemonTextElement = self.elementoText
+                destinationVC.selectedPokemon = namePoke
+                destinationVC.selectedPokemonImage = imagenSegue
+                destinationVC.selectedPokemonIconoElement = elementIcon
+                destinationVC.selectedPokemonTextElement = self.elementoText
+                
+            }
             
-        }
-
             if let stats1 = stats1, let stats2 = self.stats2, let stats3 = self.stats3, let stats4 = self.stats4, let stats5 = self.stats5, let stats6 = self.stats6{
-        
-            destinationVC.stats1 = Float(stats1 / 100)
-            destinationVC.stats2 = Float(stats2 / 100)
-            destinationVC.stats3 = Float(stats3 / 100)
-            destinationVC.stats4 = Float(stats4 / 100)
-            destinationVC.stats5 = Float(stats5 / 100)
-            destinationVC.stats6 = Float(stats6 / 100)
+                
+                destinationVC.stats1 = Float(stats1 / 100)
+                destinationVC.stats2 = Float(stats2 / 100)
+                destinationVC.stats3 = Float(stats3 / 100)
+                destinationVC.stats4 = Float(stats4 / 100)
+                destinationVC.stats5 = Float(stats5 / 100)
+                destinationVC.stats6 = Float(stats6 / 100)
                 
                 destinationVC.statsString1 = String(Int(stats1))
                 destinationVC.statsString2 = String(Int(stats2))
@@ -135,15 +133,15 @@ class ViewController: UIViewController, UITableViewDelegate {
                 destinationVC.statsString4 = String(Int(stats4))
                 destinationVC.statsString5 = String(Int(stats5))
                 destinationVC.statsString6 = String(Int(stats6))
-            
-    
+                
+                
+            }
         }
     }
-    }
-
-
-    @IBAction func closeFeatures(_ sender: UIButton) {
     
+    
+    @IBAction func closeFeatures(_ sender: UIButton) {
+        
         constrainCentre.constant = -400
         vistaFeatures.alpha = 0
         buttonViewT.alpha = 0
@@ -153,44 +151,28 @@ class ViewController: UIViewController, UITableViewDelegate {
     
     func loadItems() {
         for pokemon in 1...809 {
-                pokemonManager.fetchPokemon(pokemonCode: String(pokemon))
-            }
-            tableView.reloadData()
+            pokemonManager.fetchPokemon(pokemonCode: String(pokemon))
+        }
+        tableView.reloadData()
     }
     
     func imagenPokemonF (imagen: Int) -> String? {
-            if imagen < 10 {
-                return "00\(String(imagen))"
-            } else if imagen < 100 {
-                return "0\(String(imagen))"
-            } else if imagen < 810 {
-                return "\(String(imagen))"
-            } else {
-                 return "nil"
-            }
-    }
-    
-    func setGradientBackground() {
-        let colorTop =  UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
-        let colorBottom = UIColor(red: 222.0/255.0, green: 245.0/255.0, blue: 219.0/255.0, alpha: 1.0).cgColor
-                    
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [colorTop, colorBottom]
-        gradientLayer.locations = [0.0, 1.1]
-        gradientLayer.frame = self.view.bounds
-                
-        self.view.layer.insertSublayer(gradientLayer, at:0)
+        if imagen < 10 {
+            return "00\(String(imagen))"
+        } else if imagen < 100 {
+            return "0\(String(imagen))"
+        } else if imagen < 810 {
+            return "\(String(imagen))"
+        } else {
+            return "nil"
+        }
     }
     
     
-    
-//MARK: - TableView Delegate Methods
+    //MARK: - TableView Delegate Methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         boolVar = true
-        
-        
-    //    cell.selectedBackgroundView!.backgroundColor = .white
         
         constrainCentre.constant = 0
         vistaFeatures.alpha = 1
@@ -200,11 +182,10 @@ class ViewController: UIViewController, UITableViewDelegate {
             poke = pokemones
         } else{
             poke = pokemonesFiltrados
-            
         }
         
         if let indexPokemon = poke?[indexPath.row] {
-    
+            
             self.stats1 = Float(indexPokemon.stats1)
             self.stats2 = Float(indexPokemon.stats2)
             self.stats3 = Float(indexPokemon.stats3)
@@ -215,47 +196,46 @@ class ViewController: UIViewController, UITableViewDelegate {
             let codeP = imagenPokemonF(imagen: Int(indexPokemon.pokemonID)!)
             self.codigoPokemonFeatures.text = "#\(String(codeP!))"
             
-            
             elemento.image = UIImage(named: indexPokemon.tipoPokemon)
             
             if let imagen2 = indexPokemon.tipoPokemon2 {
-            if let elemento = UIImage(named: imagen2) {
-                elemento2?.image = elemento
+                if let elemento = UIImage(named: imagen2) {
+                    elemento2?.image = elemento
+                }
             }
-            }
-           
+            
             elementoText = "\(indexPokemon.tipoPokemon)"
-   
-        if let imagenPokemon = Int(indexPokemon.pokemonID) {
             
-            self.codigoImagenPokemon = imagenPokemonF(imagen: imagenPokemon)
-            
+            if let imagenPokemon = Int(indexPokemon.pokemonID) {
                 
-            let imagen = (UIImage(named: self.codigoImagenPokemon!))
-                     pokemonPop.setImage(imagen, for: .normal)
-                     buttonOne.setImage(imagen, for: .normal)
-                     featuresTotal.image = UIImage(named: self.codigoImagenPokemon!)
-
-            self.namePokemonFeatures.text = indexPokemon.pokemonName.capitalized
-
-             }
-    }
-        if let element = pokemones?[indexPath.row].tipoPokemon{
-        elemento.image = UIImage(named: element)
+                self.codigoImagenPokemon = imagenPokemonF(imagen: imagenPokemon)
+                
+                
+                let imagen = (UIImage(named: self.codigoImagenPokemon!))
+                pokemonPop.setImage(imagen, for: .normal)
+                buttonOne.setImage(imagen, for: .normal)
+                featuresTotal.image = UIImage(named: self.codigoImagenPokemon!)
+                
+                self.namePokemonFeatures.text = indexPokemon.pokemonName.capitalized
+                
+            }
         }
-
+        if let element = pokemones?[indexPath.row].tipoPokemon{
+            elemento.image = UIImage(named: element)
+        }
+        
         UIView.animate(withDuration: 0.1, animations: { self.view.layoutIfNeeded()})
         
         buttonViewT.alpha = 0.5
-
+        
         //MARK: - SearchBar
     }
-    }
+}
 
 
 
-    
-    
+
+
 //MARK: - PokemonManagerDelegate
 
 
@@ -263,7 +243,7 @@ extension ViewController: PokemonManagerDelegate {
     
     func didUpdatePokemon(_ pokemonManager: PokemonManager, pokemon: PokemonModel) {
         DispatchQueue.main.async {
-   
+            
             let pokemoncito: [PokemonModel] = [PokemonModel(pokemonName: pokemon.pokemonName, imageFront: pokemon.imageFront, pokemonID: pokemon.pokemonID, tipoPokemon: pokemon.tipoPokemon, abilityPokemon: pokemon.abilityPokemon, tipoPokemon2: pokemon.tipoPokemon2, stats1: pokemon.stats1, stats2: pokemon.stats2, stats3: pokemon.stats3, stats4: pokemon.stats4, stats5: pokemon.stats5, stats6: pokemon.stats6)]//, tipoPokemon2: pokemon.tipoPokemon2
             self.pokemones?.append(contentsOf: pokemoncito)
             self.pokemonesFiltrados = self.pokemones
@@ -279,45 +259,45 @@ extension ViewController: PokemonManagerDelegate {
 
 //Mark - Tableview Datasource Methods
 extension ViewController: UITableViewDataSource{
-
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return pokemonesFiltrados?.count ?? 1
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! PokemonCell
-
-    //if let item = pokemones?[indexPath.row]{
-    cell.pokemonLabel?.text = pokemonesFiltrados?[indexPath.row].pokemonName.capitalized
     
-    
-    if let codePokemon = imagenPokemonF(imagen: Int((pokemonesFiltrados?[indexPath.row].pokemonID)!)!){
-        cell.codigoPokemonLabel.text = "#\(String(codePokemon))"
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonesFiltrados?.count ?? 1
     }
     
-
-    if let imagenPokemon = Int((pokemonesFiltrados?[indexPath.row].pokemonID)!) {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        self.codigoImagenPokemon = imagenPokemonF(imagen: imagenPokemon)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! PokemonCell
         
-        if let codigo = codigoImagenPokemon{
+        //if let item = pokemones?[indexPath.row]{
+        cell.pokemonLabel?.text = pokemonesFiltrados?[indexPath.row].pokemonName.capitalized
+        
+        
+        if let codePokemon = imagenPokemonF(imagen: Int((pokemonesFiltrados?[indexPath.row].pokemonID)!)!){
+            cell.codigoPokemonLabel.text = "#\(String(codePokemon))"
+        }
+        
+        
+        if let imagenPokemon = Int((pokemonesFiltrados?[indexPath.row].pokemonID)!) {
+            
+            self.codigoImagenPokemon = imagenPokemonF(imagen: imagenPokemon)
+            
+            if let codigo = codigoImagenPokemon{
                 cell.pokemonImage.image = UIImage(named: codigo)
                 
-        
-    }
-}
-    if let tipo = pokemonesFiltrados?[indexPath.row].tipoPokemon{ //, let tipo2 =  pokemones?[indexPath.row].tipoPokemon2
-    cell.imageClaseOne.image = UIImage(named: tipo)
-   
-    }
-    
-    if let tipo2 = pokemonesFiltrados?[indexPath.row].tipoPokemon2{
-        cell.imageClaseTwo.image = UIImage(named: tipo2)
+                
+            }
         }
-    
-    return cell
-}
+        if let tipo = pokemonesFiltrados?[indexPath.row].tipoPokemon{ //, let tipo2 =  pokemones?[indexPath.row].tipoPokemon2
+            cell.imageClaseOne.image = UIImage(named: tipo)
+            
+        }
+        
+        if let tipo2 = pokemonesFiltrados?[indexPath.row].tipoPokemon2{
+            cell.imageClaseTwo.image = UIImage(named: tipo2)
+        }
+        
+        return cell
+    }
     
 }
 
@@ -341,15 +321,15 @@ extension ViewController: UISearchBarDelegate{
                 boolVar = false
                 
             }
-//            pokemonesFiltrados = pokemones?.filter({$0.pokemonName == text})
-//                pokemonesFiltrados = pokemonFil
+            //            pokemonesFiltrados = pokemones?.filter({$0.pokemonName == text})
+            //                pokemonesFiltrados = pokemonFil
         }
         
         self.tableView.reloadData()
-
+        
     }
 }
 
 
- 
+
 
